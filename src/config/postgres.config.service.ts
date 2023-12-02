132bae4d09a8ff13cce5fb3000177c6f28a5125c
Hiago Souza <hiagofss98@gmail.com>
2023-12-02 14:10:48 -0300

    Create database config service

---
+++ src/config/postgres.config.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class PostgresConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+    return {
+      type: this.configService.get('TYPEORM_TYPE'),
+      url: this.configService.get<string>('TYPEORM_URL'),
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    };
+  }
+}
