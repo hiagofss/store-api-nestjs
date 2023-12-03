8b2c6233a46b3b6141309607f497d5dac1af978e
Hiago Souza <hiagofss98@gmail.com>
2023-12-03 17:13:27 -0300

    Create structure Order domain

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ProductsModule } from './products/products.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresConfigService } from './config/postgres.config.service';
+import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { PostgresConfigService } from './config/postgres.config.service';
     }),
     UsersModule,
     ProductsModule,
+    OrderModule,
   ],
   controllers: [AppController],
   providers: [AppService],
