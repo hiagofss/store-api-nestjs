e71c7c9a1e64330747f063950ce1e05f4c6db0b2
Hiago Souza <hiagofss98@gmail.com>
2023-12-04 21:00:47 -0300

    Return jwt token to user authenticated

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
+import { UserEntity } from '../users/entities/user.entity';
+import { IUsersRespository } from '../users/repository/users.interface.respository';
+import { UsersTypeORMRepository } from '../users/repository/users.typeorm.repository';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    JwtModule.register({
+      global: true,
+      secret: 'secret',
+      signOptions: { expiresIn: '72h' },
+    }),
+  ],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  providers: [
+    AuthenticationService,
+    {
+      provide: IUsersRespository,
+      useClass: UsersTypeORMRepository,
+    },
+  ],
 })
 export class AuthenticationModule {}
--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,8 +1,38 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+  Injectable,
+  Inject,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { IUsersRespository } from 'src/users/repository/users.interface.respository';
 @Injectable()
 export class AuthenticationService {
-  auth(email: string, password: string) {
-    return 'This action adds a new authentication';
+  constructor(
+    @Inject(IUsersRespository)
+    private readonly usersRepository: IUsersRespository,
+    private jwtService: JwtService,
+  ) {}
+  async auth(email: string, password: string) {
+    const userExists = await this.usersRepository.findUserByEmail(email);
+    if (!userExists) {
+      throw new NotFoundException('User not found');
+    }
+
+    const passwordMatched = await bcrypt.compare(password, userExists.password);
+
+    if (!passwordMatched) {
+      throw new UnauthorizedException('Incorrect password');
+    }
+
+    const payload = {
+      sub: userExists.id,
+      name: userExists.name,
+    };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
